2cae9d8dbef1da88c06ee601e5e355e901920dec
alfimiftanur <alfinurhakim1234@gmail.com>
2024-03-03 20:13:59 +0700

    memperbaiki tugas 3 a

--- tugas/tugas3/3a.php
+++ tugas/tugas3/3a.php
@@ -3,9 +3,9 @@
 echo "<h4>Menghitung Luas Lingkaran</h4>";
 function hitungLuasLingkaran($r) {
     $pi = 3.14;
+    echo "Jari-jari  =  $r  cm <br>";
     return $pi * ($r **  2) ;
 }
-echo "Jari-jari  =  10  cm <br/>";
 echo "Total Luas Lingkaran Adalah = ". hitungLuasLingkaran (10) . " cm<sup>2</sup>";
 
 echo "<hr>";
@@ -13,9 +13,9 @@ echo "<hr>";
 echo "<h4>Menghitung Keliling Lingkaran</h4>";
 function hitungKelilingLingkaran($r){
     $keliling =  2 * 3.14 * $r;
+    echo "Jari-jari  = $r <br>";
     return $keliling ;
 }
-echo "Jari-jari  = 20 cm <br/>";
 echo "Total Keliling Lingkaran Adalah = ". hitungKelilingLingkaran (20) . " cm";
 echo "<hr>";
 
